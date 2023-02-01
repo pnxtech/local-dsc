2aac27a117076e5c3753004d536b91bdc726b6cb
cjus <Carlos.Justiniano@gmail.com>
2023-02-01 10:44:54 -0700

    update for linux version of redis-debug script

--- scripts/redis-debug.sh
+++ scripts/redis-debug.sh
@@ -1,3 +1,3 @@
-$REDIS_ID=$(docker ps -qf "name=redis")
+source ./stack_name.sh
+REDIS_ID=$(docker ps -qf "name=${STACK_NAME}_redis")
 docker exec -it ${REDIS_ID} /usr/bin/redis-cli --ldb --eval $1
-
