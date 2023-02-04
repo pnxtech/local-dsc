667c9507fb7c0475d14dbe63a07f326f849f7e48
cjus <Carlos.Justiniano@gmail.com>
2023-02-04 09:47:43 -0700

    update lua script debugger

--- scripts/redis-debug.sh
+++ scripts/redis-debug.sh
@@ -1,3 +1,3 @@
 source ./stack_name.sh
 REDIS_ID=$(docker ps -qf "name=${STACK_NAME}_redis")
-docker exec -it ${REDIS_ID} /usr/bin/redis-cli --ldb --eval $1
+docker exec -it ${REDIS_ID} /usr/bin/redis-cli --ldb --eval scripts/$1
